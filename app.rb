8ac300440710b2f3b948b2914ff386c8b22ce994
curly08 <mattynoakes08@gmail.com>
2021-09-22 12:53:50 -0400

    Add classes for each noun in the game

---
+++ app.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+# Player class
+class Player
+end
+
+# Codebreaker class
+class Codebreaker
+end
+
+# Codemaker class
+class Codemaker
+end
+
+# DecodingBoard class
+class DecodingBoard
+end
+
+# CodePegs class
+class CodePegs
+end
+
+# CodePegs subclass for each of the six different colors?
+
+# KeyPegs class
+class KeyPegs
+end
+
+# BlackKeyPegs class
+class BlackKeyPegs
+end
+
+# WhiteKeyPegs class
+class WhiteKeyPegs
+end
