0fd9bb7295791454ff6733f0cf5e983a875bcd17
curly08 <mattynoakes08@gmail.com>
2021-09-22 15:53:43 -0400

    Encapsulate in Mastermind module, add establish_codemaker and establish_codebreaker methods

--- app.rb
+++ app.rb
@@ -1,35 +1,67 @@
 # frozen_string_literal: true
 
-# Player class
-class Player
+# Mastermind module
+module Mastermind
+  # Game class
+  class Game
+    def initialize(codemaker, codebreaker)
+      @codemaker = codemaker
+      @codebreaker = codebreaker
+    end
+  end
+
+  # Player class
+  class Player
+    attr_reader :name
+
+    def initialize(name)
+      @name = name
+    end
+  end
+
+  # Codebreaker class
+  class Codebreaker < Player
+  end
+
+  # Codemaker class
+  class Codemaker < Player
+  end
+
+  # DecodingBoard class
+  class DecodingBoard
+  end
+
+  # CodePegs class
+  class CodePegs
+  end
+
+  # CodePegs subclass for each of the six different colors?
+
+  # KeyPegs class
+  class KeyPegs
+  end
+
+  # BlackKeyPegs class
+  class BlackKeyPegs
+  end
+
+  # WhiteKeyPegs class
+  class WhiteKeyPegs
+  end
+
+  def establish_codemaker
+    puts 'Who will be the codemaker?'
+    Codemaker.new(gets.chomp)
+  end
+
+  def establish_codebreaker
+    puts 'Who will be the codebreaker?'
+    Codebreaker.new(gets.chomp)
+  end
 end
 
-# Codebreaker class
-class Codebreaker
-end
+include Mastermind
 
-# Codemaker class
-class Codemaker
-end
-
-# DecodingBoard class
-class DecodingBoard
-end
-
-# CodePegs class
-class CodePegs
-end
-
-# CodePegs subclass for each of the six different colors?
-
-# KeyPegs class
-class KeyPegs
-end
-
-# BlackKeyPegs class
-class BlackKeyPegs
-end
-
-# WhiteKeyPegs class
-class WhiteKeyPegs
-end
+codemaker = establish_codemaker
+codebreaker = establish_codebreaker
+Game.new(codemaker, codebreaker)
