0cb825f38f97c81ea9bd6bc9d6490ed9fae4108d
curly08 <mattynoakes08@gmail.com>
2021-09-23 12:42:43 -0400

    Add GuessAttempt class for guess attempt instances, storing guess attempts and number of guess attempts in class variables

--- app.rb
+++ app.rb
@@ -4,10 +4,10 @@
 module Mastermind
   # Game class
   class Game
-    attr_reader :code
+    attr_reader :code, :codebreaker
 
-    def initialize(codebreaker)
-      @codebreaker = codebreaker
+    def initialize
+      @codebreaker = Player.new.name
       @code = Code.new.code
     end
   end
@@ -16,8 +16,9 @@ module Mastermind
   class Player
     attr_reader :name
 
-    def initialize(name)
-      @name = name
+    def initialize
+      puts 'What is your name, codebreaker?'
+      @name = gets.chomp
     end
 
     # attempt to guess code
@@ -25,9 +26,9 @@ module Mastermind
 
   # Code class
   class Code
-    @@possible_code_values = %w[blue red green pink yellow purple]
-
     attr_reader :code
+
+    @@possible_code_values = %w[blue red green pink yellow purple]
 
     def initialize(code = randomize_code)
       @code = code
@@ -35,6 +36,29 @@ module Mastermind
 
     def randomize_code
       4.times.map { @@possible_code_values.sample }
+    end
+  end
+
+  # Guess Attempt class
+  class GuessAttempt
+    attr_reader :guess
+
+    @@num_of_guesses = 0
+    @@guess_attempts = []
+
+    def initialize
+      puts 'What is your guess?'
+      @guess = gets.chomp.split(' ')
+      @@guess_attempts << @guess
+      @@num_of_guesses += 1
+    end
+
+    def self.guess_attempts
+      @@guess_attempts
+    end
+
+    def self.num_of_guesses
+      @@num_of_guesses
     end
   end
 
@@ -71,6 +95,6 @@ end
 
 include Mastermind
 
-# new_game = Game.new('matt')
-code = Code.new
-puts code.code
+# new_game = Game.new
+# puts new_game.code
+# puts new_game.codebreaker
